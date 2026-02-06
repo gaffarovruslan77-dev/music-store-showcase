d0ee6c16972a134b723f6fe871ea66987ed189ef
Ruslan <gaffarovruslan77@gmail.com>
2026-02-07 01:12:49 +0500

    Initial Create: Music Store Showcase

---
+++ MusicStoreShowcase/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MusicStoreShowcase.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
